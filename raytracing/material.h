1b12ce73480de4eff3e10d11b6c9e5a6bc547770
Shiwen <shiwenbot@gmail.com>
2023-08-14 20:53:59 +0800

    Add project files.

---
+++ raytracing/material.h
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "rtweekend.h"
+#include "hittable.h"
+
+struct hit_record;
+
+class material {
+public:
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+    ) const = 0;
+};
+
+class lambertian : public material {
+public:
+    lambertian(const vec3& a) : albedo(a) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+    ) const {
+        vec3 scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+public:
+    vec3 albedo;
+};
+
+class metal : public material {
+public:
+    metal(const vec3& a) : albedo(a) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+    ) const {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return (dot(scattered.direction(), rec.normal) > 0);
+    }
+
+public:
+    vec3 albedo;
+};
+#endif
